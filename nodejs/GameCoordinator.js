8a8eaba734b64814e072fae47beb1428d568e716
Andy <Sunstradamus@users.noreply.github.com>
2015-11-09 18:27:34 -0800

    GC can now start lobbies, GS can now quit

--- nodejs/GameCoordinator.js
+++ nodejs/GameCoordinator.js
@@ -12,8 +12,11 @@ switch (mode) {
     break;
   // Run as both a master and cluster GC server
   case 'combined':
-    var http = require('http'),
+    var exec = require('exec'),
+        http = require('http'),
         parser = require('url').parse;
+
+    var lobbies = {};
     http.createServer(function(req, res) {
       var url = parser(req.url, true);
       res.setHeader("Content-Type", "application/json; charset=UTF-8");
@@ -24,10 +27,23 @@ switch (mode) {
               res.end();
               break;
             case '/new_lobby':
-              res.write('test');
+              var id = "";
+              var allowed = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+              for (var i = 0; i < 5; i++) {
+                id += allowed.charAt(Math.floor(Math.random() * 36));
+              }
+              exec('node /home/vagrant/project/nodejs/GameServer.js '+id, function (error) {
+                if (error !== null) {
+                  res.write(JSON.stringify({ error: "Could not start GameServer" }));
+                } else {
+                  lobbies[id] = { state: 0 };
+                  res.write(JSON.stringify({ id: id, state: 0 }));
+                }
+              });
               res.end();
               break;
             case '/lobbies':
+              res.write(JSON.stringify(lobbies));
               res.end();
             default:
               res.end();
@@ -37,6 +53,16 @@ switch (mode) {
         case 'POST':
           switch(url.pathname) {
             case '/user_token':
+              res.end();
+              break;
+            default:
+              res.end();
+              break;
+          }
+          break;
+        case 'DELETE':
+          switch(url.pathname) {
+            case '/lobby':
               res.end();
               break;
             default:
