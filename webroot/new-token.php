5f11e87ed1a58cbfeaf6e86314f3c82fbd937505
Andy <mrandysun+gitlab@gmail.com>
2015-12-05 11:47:07 -0800

    JSONify

--- webroot/new-token.php
+++ webroot/new-token.php
@@ -3,4 +3,4 @@ require_once '/home/vagrant/project/phpcore/core.inc.php';
 authenticate_user();
 
 generate_user_token();
-echo $_SESSION['user']['token'];
+echo json_encode(array("token" => $_SESSION['user']['token']));
